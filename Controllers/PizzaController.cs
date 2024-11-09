d70fea5bffdbef58b0e9d98d130294d4d14ab805
carobte <caro.bustamante.escobar@gmail.com>
2024-11-09 12:56:05 -0500

    feat: create, update and delete methods implemented

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -30,15 +30,48 @@ public class PizzaController : ControllerBase
     [HttpGet("{id}")]
     public ActionResult<Pizza?> GetId(int id)
     {
-
         var pizza = PizzaService.Get(id);
         return pizza == null ? NotFound() : Ok(pizza);
-
     }
 
     // POST
+    [HttpPost]
+    public IActionResult Create(Pizza pizza)
+    {
+        PizzaService.Add(pizza);
+        // CreatedAtAction not only returns a 201(created) but also includes the location of the 
+        // newly created resource in the response header (in the Location field)
+        return CreatedAtAction(nameof(GetId), new { id = pizza.Id }, pizza);
+    }
 
     // PUT
 
+    [HttpPut("{id}")]
+    public IActionResult Update(int id, Pizza pizza)
+    {
+        if (id != pizza.Id)
+            return BadRequest(); // id is different from the proporcioned pizza.Id 
+
+        var existingPizza = PizzaService.Get(id);
+        if (existingPizza == null)
+            return NotFound(); // no pizza found
+
+        PizzaService.Update(pizza);
+
+        return NoContent();
+    }
+
     // DELETE
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+        var pizza = PizzaService.Get(id);
+
+        if (pizza == null)
+            return NotFound(); // no pizza found
+
+        PizzaService.Delete(id);
+
+        return NoContent();
+    }
 }
